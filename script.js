53d25ecb2d3843921321af4020186dbbb22ede58
Razaberry63 <66958460+Razaberry63@users.noreply.github.com>
2021-05-30 07:30:35 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
 //Slider Initialization
-
- $('.home-slider').owlCarousel({
-    loop: true,
-    autoplay: true,
-    margin: 10,
-    nav: true,
-    autoplayHoverPause: true,
-    item: 1,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    navText: ["<span class='fa fa-chevron-left'>","</span>","<span class='fa-fa-Chevron-right'></span>"],
-    responsive: {
-    0:{
-        items: 1,
-        nav: false
-    },
-    600:{
-        items: 1,
-        nav: false
-    },
-    1000:{
-        items: 1,
-        nav: true
-    }
-}
-})
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
